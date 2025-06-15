ea489ccb2bd2bc017539673ac97c7443564d4015
German David <germanmarinrolong@gmail.com>
2025-06-14 22:50:56 -0500

    improve header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
 import { routes } from "@/lib/routes";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
 
 export function Header() {
+    const pathname = usePathname();
+
     return (
         <header className="flex h-[55px] flex-row items-center justify-between gap-2 border-b-1 border-r-slate-400 px-2 py-4">
             <div className="flex flex-row items-center justify-start gap-2">
@@ -15,7 +20,11 @@ export function Header() {
                         <Button
                             asChild
                             key={route.href}
-                            variant="secondary"
+                            variant={
+                                pathname === route.href
+                                    ? "default"
+                                    : "secondary"
+                            }
                             size="sm"
                         >
                             <Link href={route.href}>{route.label}</Link>
